3bce9a07bfec8462269fad301cce31f61f5c40c6
Vincent Barresi <vbarresi@amplify.com>
2015-08-04 00:03:51 -0400

    add algorithm to return index # at key

---
+++ return_index_at_key.rb
@@ -0,0 +1,19 @@
+=begin
+  Return index of sorted array at a key value
+=end
+
+def index_at_key key
+  puts 'key is: ' + key.to_s
+  arr = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+  i, j, key_arr = 0, 0, []
+  while i <= arr.size - 1
+    if arr[i] == key
+      key_arr[j] = i
+      j += 1
+    end
+    i += 1
+  end
+  p 'key appears at index #: ' + key_arr.join(", ")
+end
+
+index_at_key rand(1..10)
