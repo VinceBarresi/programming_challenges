31da7c2641bd40b0e71e39928426d5993a36ec45
Vincent Barresi <vbarresi@amplify.com>
2015-08-08 11:13:59 -0400

    replace while with upto block and replace if cond with ternary

--- cap_and_reverse_alternate_words.rb
+++ cap_and_reverse_alternate_words.rb
@@ -5,15 +5,10 @@
 =end
 
 def cap_and_reverse_alternate str
-  arr_string, i, arr, = str.split, 0, []
-  while i <= arr_string.size - 1
+  arr_string, arr, = str.split, []
+  0.upto(arr_string.size - 1) do |i|
     current_word = arr_string[i]
-    if i % 2 == 0
-      arr[i] = current_word.reverse
-    else
-      arr[i] = current_word.swapcase
-    end
-    i += 1
+    i % 2 == 0 ? arr[i] = current_word.reverse : arr[i] = current_word.swapcase
   end
   puts arr.join(" ")
 end
