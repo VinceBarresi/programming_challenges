44f17f97212b7ebdec61c9dd1d0eb1662723df65
Vincent Barresi <vbarresi@amplify.com>
2015-08-09 22:34:43 -0400

    update .rspec and remove deprecated methods from specs

--- spec/cap_and_reverse_alternate_words_spec.rb
+++ spec/cap_and_reverse_alternate_words_spec.rb
@@ -4,11 +4,11 @@ require_relative '../cap_and_reverse_alternate_words'
 describe 'capitalize and reverse alternate words of a string' do
   it 'should take a string and capitalize and reverse alternate words' do
     str = 'i love unit tests'
-    cap_and_reverse_alternate(str).should eq('i LOVE tinu TESTS')
+    expect(cap_and_reverse_alternate(str)).to eq('i LOVE tinu TESTS')
   end
 
   it 'should not reverse odd placed words' do
     str = 'unit tests are fun'
-    cap_and_reverse_alternate(str).should_not eq('UNIT stset ARE nuf')
+    expect(cap_and_reverse_alternate(str)).not_to eq('UNIT stset ARE nuf')
   end
 end
--- spec/even_odd_index_spec.rb
+++ spec/even_odd_index_spec.rb
@@ -1,13 +1,14 @@
+require_relative 'spec_helper'
 require_relative '../even_odd_index'
 
 describe 'even and odd values of array' do
   it 'should sort even values in even indeces and odd values in odd indeces' do
     arr = [1, 2, 3, 4, 5, 6]
-    even_odd(arr).should eq([2, 1, 4, 3, 6, 5])
+    expect(even_odd(arr)).to eq([2, 1, 4, 3, 6, 5])
   end
 
   it 'should not sort even values into odd indeces' do
     arr = [1, 2, 3, 4, 5, 6]
-    even_odd(arr).should_not eq([1, 2, 3, 4, 5, 6])
+    expect(even_odd(arr)).to_not eq([1, 2, 3, 4, 5, 6])
   end
 end
