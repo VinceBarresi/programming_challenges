96268360f0053fd28da886e499accb8d3db7ce10
Vincent Barresi <vbarresi@amplify.com>
2015-08-11 22:57:52 -0400

    remove print from method

---
+++ add_two_ints_without_+.rb
@@ -0,0 +1,18 @@
+=begin
+  Write code to add two integers with using +, you
+  can only use ++ or --.
+  BONUS
+  How would you handle negative integers?
+=end
+
+def sum a, b
+  puts 'a is '"#{a}", 'b is '"#{b}"
+  while b != 0
+    carry = a & b # bitwise AND of two
+    a = a^b # binary XOR - copies bit to a from b if not set in b
+    b = carry << 1
+  end
+  a
+end
+
+sum rand(20), rand(100)
