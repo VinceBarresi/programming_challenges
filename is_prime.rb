0efaf3e1fcffb74fbc0f5449d5a3d02862603a71
Vincent Barresi <vbarresi@amplify.com>
2015-08-11 22:52:56 -0400

    update and complete algorithm

--- is_prime.rb
+++ is_prime.rb
@@ -5,11 +5,14 @@
 def is_prime? n
   return true if n == 2
   return false if n < 2 || n % 2 == 0
-  i, bool = 1, ''
-  n.times do
-    bool = n % i == 0 ? true : false
+  i = 3
+  while i <= Math.sqrt(n) do
+    if n % i == 0
+      return false
+    end
+   i += 2
   end
-  return bool
+  return true
 end
 
-p is_prime? 9
+p is_prime? 15
