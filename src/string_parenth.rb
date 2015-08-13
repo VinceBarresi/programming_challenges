620731aeb954e5bcf08b50adf3c4abc39fcbf999
Vincent Barresi <vbarresi@amplify.com>
2015-08-12 22:15:13 -0400

    add valid string with matching parenth?

---
+++ src/string_parenth.rb
@@ -0,0 +1,19 @@
+=begin
+  Check if the parentheses in a string are valid,
+  or invalid.
+=end
+
+def valid_parenths str
+  str, lft_par, rgt_par, i = str.split(''), 0, 0, 0
+  while i <= str.size - 1
+    if str[i] == '('
+      lft_par += 1
+    elsif str[i] == ')'
+      rgt_par += 1
+    end
+    i += 1
+  end
+  lft_par == rgt_par ? 'valid string' : 'invalid string'
+end
+
+valid_parenths '((hello)'
