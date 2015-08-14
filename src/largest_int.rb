5c474c02eec33c55fb187a98594d07e2fc0152ef
Vincent Barresi <vbarresi@amplify.com>
2015-08-14 18:38:26 -0400

    add solution to largest int from int digits

---
+++ src/largest_int.rb
@@ -0,0 +1,10 @@
+=begin
+  Given an integer, find the maximum number
+  that could be formed from the digits.
+=end
+
+def largest_int x
+  x.to_s.split("").sort.reverse.join.to_i
+end
+
+p largest_int 2912942992124
