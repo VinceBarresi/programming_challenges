0d8119126b091b0cef43ddc58802f3caa9801b8c
Vincent Barresi <vbarresi@amplify.com>
2015-08-18 16:13:02 -0400

    modify expect to use truthy and falsy

--- spec/is_prime_spec.rb
+++ spec/is_prime_spec.rb
@@ -3,10 +3,10 @@ require_relative '../src/is_prime'
 
 describe 'is prime' do
   it 'returns true if a number is prime' do
-    expect(is_prime?(3)).to eq(true)
+    expect(is_prime?(3)).to be_truthy
   end
 
   it 'returns false if a number is not prime' do
-    expect(is_prime?(9)).to eq(false)
+    expect(is_prime?(9)).to be_falsy
   end
 end
