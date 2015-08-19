e8475e5342c76d5d23e84704e7c7231fc044a531
Vincent Barresi <vbarresi@amplify.com>
2015-08-18 21:14:44 -0400

    add solution for return character with the most appearances in a string

---
+++ src/most_character.rb
@@ -0,0 +1,16 @@
+=begin
+  Given a string, determine which character
+  appears the most and print it.
+=end
+
+def most_char str
+  max, letter, i, count, str_arr, = 0, '', 0, [], str.split("")
+    str_arr.each do |c|
+      count += [str.count(c)]
+      max, letter = count[i], str_arr[i] if count[i] > max
+    i += 1
+  end
+  letter
+end
+
+puts most_char 'ihhasakkkkkl'
