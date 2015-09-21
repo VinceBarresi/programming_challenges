c8bfdf4e22a1ea0a149e54698cd99ee43cfff021
vincebarresi <vincebarresi@gmail.com>
2015-09-21 19:50:46 -0400

    clean up code

--- src/remove_duplicates_from_strings.rb
+++ src/remove_duplicates_from_strings.rb
@@ -6,13 +6,8 @@
 def remove_duplicates str1, str2
   bigger = (str1.length > str2.length ? str1 : str2).split('')
   smaller = (bigger == str1 ? str2 : str1).split('')  
-    bigger.each do |c|
-      if smaller.include?(c)
-        bigger.delete(c)
-        smaller.delete(c)
-      end
-    end
-   smaller.join + ' ' + bigger.join
+  bigger.each {|c| bigger.delete(c) && smaller.delete(c) if smaller.include?(c)}
+  smaller.join + ' ' + bigger.join
 end
 
 p remove_duplicates 'abc', 'cde'
