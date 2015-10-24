62c9f8b7819b37d3b34f09c03d3f54c82a0ab3b0
vincebarresi <vincebarresi@gmail.com>
2015-10-24 18:32:35 -0400

    clean up code to reduce lines

--- src/group_anagrams.rb
+++ src/group_anagrams.rb
@@ -8,9 +8,7 @@
 def group_anagrams arr = []
   sort_arr, sort_hash, new_sort_arr = [], {}, []
   arr = arr.each {|w| sort_arr << w.split('').sort.join}
-  0.upto(arr.size-1) do |i|
-    sort_hash.store(arr[i], sort_arr[i])
-  end
+  0.upto(arr.size-1) { |i| sort_hash.store(arr[i], sort_arr[i]) }
   new_sort_arr << sort_hash.sort_by {|k, v| k.length}.to_h.keys[0..arr.size-1]
 end 
 
