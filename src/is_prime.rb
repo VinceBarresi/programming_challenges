c8b092b9d261bdb737e2a3a878d21961d6f494e6
Vincent Barresi <vincebarresi@gmail.com>
2015-10-24 18:24:18 -0400

    clean up while

--- src/is_prime.rb
+++ src/is_prime.rb
@@ -7,10 +7,8 @@ def is_prime? n
   return false if n < 2 || n % 2 == 0
   i = 3
   while i <= Math.sqrt(n) do
-    if n % i == 0
-      return false
-    end
-   i += 2
+    return false if n % i == 0
+    i += 2
   end
   return true
 end
