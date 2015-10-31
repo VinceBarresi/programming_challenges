b2f6433eab0d0c1135ae960a2cc76cc925bf83bc
Vincent Barresi <vincebarresi@gmail.com>
2015-10-30 23:00:27 -0400

    Update sum_of_digits.rb

--- src/sum_of_digits.rb
+++ src/sum_of_digits.rb
@@ -1,16 +1,14 @@
 =begin
-	Given a string of integers,
-	print the sum.
+  Given a string of integers,
+  print the sum.
 =end
 
 # O(n)	
 def sum_string int_str
-	x = 0
-	int_str.split('').each do |y|
-		y = y.to_i
-		x += y
-	end
-	x
+  x = 0
+  int_str.split('').each do |y|
+    y = y.to_i
+    x += y
+  end
+  x
 end
-
-p sum_string '13579'
