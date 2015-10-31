001cf079f80138edf214e27f40c20d169ea705b0
Vincent Barresi <vincebarresi@gmail.com>
2015-10-30 22:48:26 -0400

    Update int_to_linked_list.rb

--- src/int_to_linked_list.rb
+++ src/int_to_linked_list.rb
@@ -1,8 +1,8 @@
 =begin
-	Write a function that takes input as integer and converts 
-	it into a linked list where each node represents a digit of the input integer. 
-	e.g. i/p : 123 
-	expected result: [1] => [2] => [3]
+  Write a function that takes input as integer and converts 
+  it into a linked list where each node represents a digit of the input integer. 
+  e.g. i/p : 123 
+  expected result: [1] => [2] => [3]
 =end
 
 require_relative 'linked_list'
@@ -10,13 +10,11 @@ require_relative 'list_node'
 
 # O(n)
 def int_to_list int
-	int_arr, x = int.to_s.split(''), 1
-	int_list = LinkedList.new(int_arr.first)
-	int_arr.size.times do
-		int_list.add(int_arr[x])
-		x+=1
-	end
-	int_list.display
+  int_arr, x = int.to_s.split(''), 1
+  int_list = LinkedList.new(int_arr.first)
+  int_arr.size.times do
+    int_list.add(int_arr[x])
+    x+=1
+  end
+int_list.display
 end
-
-p int_to_list 123
