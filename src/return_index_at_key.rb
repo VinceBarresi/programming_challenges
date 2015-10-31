7252b165f1c1f016818256bc5b1cf87790519cac
Vincent Barresi <vincebarresi@gmail.com>
2015-10-30 22:59:06 -0400

    Update return_index_at_key.rb

--- src/return_index_at_key.rb
+++ src/return_index_at_key.rb
@@ -2,8 +2,6 @@
   Return index of sorted array at a key value
 =end
 
-arr = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
-
 # O(n)
 def index_at_key key, arr
   puts 'key is: ' + key.to_s
@@ -17,5 +15,3 @@ def index_at_key key, arr
   end
   key_arr.join(', ')
 end
-
-index_at_key rand(1..10), arr
