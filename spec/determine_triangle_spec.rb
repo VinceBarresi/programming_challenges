82dbe4e97fe3f3ec574036b3f3994fc8800340cc
Vincent Barresi <vincebarresi@gmail.com>
2016-04-13 14:13:23 -0400

    Update determine_triangle_spec.rb

--- spec/determine_triangle_spec.rb
+++ spec/determine_triangle_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../src/determine_triangle'
 
 describe 'determine_triangle' do
-	it 'returns scalene if the trianle has 3 unequal sides' do
-		expect(determine_triangle).to eq
-	end
+  it 'returns scalene if the trianle has 3 unequal sides' do
+    expect(determine_triangle).to eq
+  end
 end
