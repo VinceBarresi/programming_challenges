4396c3e8d1fe543148407f59b13e9c04ab89d82b
Vincent Barresi <vincebarresi@gmail.com>
2016-04-13 14:11:17 -0400

    Update number_of_primes_spec.rb

--- spec/number_of_primes_spec.rb
+++ spec/number_of_primes_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 require_relative '../src/number_of_primes'
 
 describe 'number_of_primes' do
-	it 'returns the number of primes in a given range' do
-		expect(number_of_primes(2..11)).to eq(5)
-	end
+  it 'returns the number of primes in a given range' do
+    expect(number_of_primes(2..11)).to eq(5)
+  end
 end
