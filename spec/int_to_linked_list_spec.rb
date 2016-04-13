5e5f6c7de83e022e41330515721b41807851a9fc
Vincent Barresi <vincebarresi@gmail.com>
2016-04-13 14:12:13 -0400

    Update int_to_linked_list_spec.rb

--- spec/int_to_linked_list_spec.rb
+++ spec/int_to_linked_list_spec.rb
@@ -3,7 +3,7 @@ require_relative '../src/list_node'
 require_relative '../src/int_to_linked_list'
 
 describe 'int_to_list' do
-	it 'converts an int to a linked list'	do
-		expect(int_to_list(12345)).to eq('1->2->3->4->5->')
-	end
+  it 'converts an int to a linked list'	do
+    expect(int_to_list(12345)).to eq('1->2->3->4->5->')
+  end
 end
